3cea682244a98f1a17d5e775033bb5ba29cc8b53
Tom <jamaszi@gmail.com>
2021-06-15 16:49:01 +0200

    QuizListComponent

--- TestMaker/TestMakerWeb/ClientApp/src/app/app.module.ts
+++ TestMaker/TestMakerWeb/ClientApp/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
+import { QuizListComponent } from './quiz/quiz-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
-    HomeComponent
+    HomeComponent,
+    QuizListComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
