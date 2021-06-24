c72830c60e4280a0ea7f474c78c5becb4be2441c
Tom <jamaszi@gmail.com>
2021-06-24 18:33:32 +0200

    rozbudowanie QuizListComponent o możliwość konfiguracji

--- TestMaker/TestMakerWeb/ClientApp/src/app/quiz/quiz-list.component.ts
+++ TestMaker/TestMakerWeb/ClientApp/src/app/quiz/quiz-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, Input, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -7,16 +7,42 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./quiz-list.component.css']
 })
 
-export class QuizListComponent {
+export class QuizListComponent implements OnInit {
+  @Input() class: string;
   title: string;
   selectedQuiz: Quiz;
   quizzes: Quiz[];
+  http: HttpClient;
+  baseUrl: string;
 
   constructor(http: HttpClient,
     @Inject('BASE_URL') baseUrl: string) {
-    this.title = "Najnowsze quizy";
-    var url = baseUrl + "api/quiz/Latest";
-    http.get<Quiz[]>(url).subscribe(result => {
+    this.http = http;
+    this.baseUrl = baseUrl;
+  }
+
+  ngOnInit() {
+    console.log("QuizListComponent " + "został utworzony z użyciem następującej klasy: " + this.class);
+
+    var url = this.baseUrl + "api/quiz/";
+
+    switch (this.class) {
+      case "latest":
+      default:
+        this.title = "Najnowsze";
+        url += "Latest/";
+        break;
+      case "byTitle":
+        this.title = "Alfabetycznie";
+        url += "ByTitle/";
+        break;
+      case "random":
+        this.title = "Losowe"
+        url += "Random/";
+        break;
+    }
+
+    this.http.get<Quiz[]>(url).subscribe(result => {
       this.quizzes = result;
     }, error => console.error(error));
   }
