38ebf4a48fd7e26dff2be8fcb010f1d0f19e736b
Tom <jamaszi@gmail.com>
2021-07-01 20:17:48 +0200

    szkielet encji

---
+++ TestMaker/TestMakerWeb/Data/Models/Result.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TestMakerWeb.Data
+{
+  public class Result
+  {
+    #region Konstruktor
+    public Result()
+    {
+
+    }
+    #endregion
+
+    #region Właściwości
+    [Key]
+    [Required]
+    public int Id { get; set; }
+
+    [Required]
+    public int QuizId { get; set; }
+
+    [Required]
+    public string Text { get; set; }
+
+    public int? MinValue { get; set; }
+
+    public int? MaxValue { get; set; }
+
+    public string Notes { get; set; }
+
+    [DefaultValue(0)]
+    public int Type { get; set; }
+
+    [DefaultValue(0)]
+    public int Flags { get; set; }
+
+    [Required]
+    public DateTime CreatedDate { get; set; }
+
+    [Required]
+    public DateTime LastModifiedDate { get; set; }
+    #endregion
+  }
+}
