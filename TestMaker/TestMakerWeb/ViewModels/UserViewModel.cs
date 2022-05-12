52e0fb79bd197b06c29cfe098eb37ddda068fdab
Tom <jamaszi@gmail.com>
2022-05-12 13:56:59 +0200

    dodanie UserController

---
+++ TestMaker/TestMakerWeb/ViewModels/UserViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestMakerWeb.ViewModels
+{
+  [JsonObject (MemberSerialization.OptOut)]
+  public class UserViewModel
+  {
+    #region Konstruktor
+    public UserViewModel()
+    {
+
+    }
+    #endregion
+
+    #region Właściwości
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public string Email { get; set; }
+    public string DisplayName { get; set; }
+    #endregion
+  }
+}
